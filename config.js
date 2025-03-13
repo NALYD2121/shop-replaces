bf38e86f6eb3fd1360e3cff396cbb942749da347
NALYD2121 <nalyd.24nr@gmail.com>
2025-03-13 13:00:38 +0100

    z

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Configuration de l'API
-window.API_URL = 'https://cb4dfe0d-220b-4edd-a87c-0d30661b7aaf-00-23bw0eqpwil9w.picard.replit.dev/api';
+window.API_URL = 'https://worker-production-26e1.up.railway.app/api';
--- js/mods.js
+++ js/mods.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_URL = 'https://cb4dfe0d-220b-4edd-a87c-0d30661b7aaf-00-23bw0eqpwil9w.picard.replit.dev/api';
+const API_URL = 'https://worker-production-26e1.up.railway.app/api';
 const CHANNEL_IDS = {
     ARME: {
         'AWP MK2': '1339958173125050422',
